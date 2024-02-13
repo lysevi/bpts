115806e3137824ef0995f052817b86dc35cb8b30
Любимов Сергей Викторович <sergey.lubimov@monitel.ru>
2024-02-13 11:33:09 +0300

    bpts::find

--- bpts/src/lib.rs
+++ bpts/src/lib.rs
@@ -1,28 +1,53 @@
 pub mod mfile;
 pub mod rec;
+pub mod types;
+
+use std::collections::HashMap;
 
 use rec::Record;
+use types::Id;
 
-pub struct Node{
-    pub id:u32,
-    pub is_leaf:bool,
+#[derive(Clone)]
+pub struct Node {
+    pub id: Id,
+    pub is_leaf: bool,
     pub keys: Vec<i32>,
-    pub data: Vec<Record>
+    pub data: Vec<Record>,
 }
 
-impl Node{
-
-    pub fn new(is_leaf:bool, keys:Vec<i32>, data:Vec<Record>) -> Node{
-        Node { id: 0, is_leaf: is_leaf, keys: keys, data: data }
+impl Node {
+    pub fn new(id: Id, is_leaf: bool, keys: Vec<i32>, data: Vec<Record>) -> Node {
+        Node {
+            id: id,
+            is_leaf: is_leaf,
+            keys: keys,
+            data: data,
+        }
     }
 
-    pub fn new_leaf(keys:Vec<i32>, data:Vec<Record>) -> Node{
-        Node::new(true, keys, data)
+    pub fn new_root(id: Id, keys: Vec<i32>, data: Vec<Record>) -> Node {
+        Node::new(id, false, keys, data)
     }
 
-    pub fn find(&self, key:i32) -> Option<&Record>{
-        for i in 0..self.keys.len(){
-            match (self.keys[i]).cmp(&key){
+    pub fn new_leaf(id: Id, keys: Vec<i32>, data: Vec<Record>) -> Node {
+        Node::new(id, true, keys, data)
+    }
+
+    pub fn find(&self, key: i32) -> Option<&Record> {
+        if key < self.keys[0] {
+            return self.data.first();
+        }
+
+        if !self.is_leaf {
+            if let Some(last_key) = self.keys.last() {
+                if *last_key <= key {
+                    return self.data.last();
+                }
+            }
+        }
+
+        for i in 0..self.keys.len() {
+            match (self.keys[i]).cmp(&key) {
                 std::cmp::Ordering::Less => continue,
                 std::cmp::Ordering::Equal => return Some(&self.data[i]),
                 std::cmp::Ordering::Greater => return Some(&self.data[i]),
@@ -31,17 +56,112 @@ impl Node{
         return None;
     }
 }
+
+pub trait NodeStorage {
+    fn get_node(&self, id: Id) -> Option<&Node>;
+    fn add_node(&mut self, node: &Node);
+}
+
+pub struct MockNodeStorage {
+    nodes: HashMap<Id, Node>,
+}
+
+impl MockNodeStorage {
+    pub fn new() -> MockNodeStorage {
+        MockNodeStorage {
+            nodes: HashMap::new(),
+        }
+    }
+}
+impl NodeStorage for MockNodeStorage {
+    fn get_node(&self, id: Id) -> Option<&Node> {
+        self.nodes.get(&id)
+    }
+
+    fn add_node(&mut self, node: &Node) {
+        self.nodes.insert(node.id, node.clone());
+    }
+}
+
+// TODO to Result<>
+pub fn find<'a>(storage: &'a dyn NodeStorage, root: &'a Node, key: i32) -> Option<&'a Record> {
+    let mut target = root;
+
+    loop {
+        let rec = target.find(key);
+        if target.is_leaf {
+            return rec;
+        }
+        if rec.is_none() {
+            break;
+        }
+        let node_id = rec.unwrap().into_id();
+        let tmp = storage.get_node(node_id);
+        if tmp.is_none() {
+            panic!("{:?} not found", node_id)
+        }
+        target = tmp.unwrap();
+    }
+    return None;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-   
     #[test]
-    fn leaf_find() {
-        let  leaf=Node::new_leaf(vec![1,2,3,4], vec![Record::from_u8(1), Record::from_u8(2),Record::from_u8(3),Record::from_u8(4)]);
-        if let Some(item)=leaf.find(2){
-            let v= item.into_u8();
+    fn find_in_leaf() {
+        let leaf = Node::new_leaf(
+            0,
+            vec![1, 2, 3, 4],
+            vec![
+                Record::from_u8(1),
+                Record::from_u8(2),
+                Record::from_u8(3),
+                Record::from_u8(4),
+            ],
+        );
+        if let Some(item) = leaf.find(2) {
+            let v = item.into_u8();
             assert_eq!(v, 2u8);
         }
+
+        if let Some(item) = leaf.find(1) {
+            let v = item.into_u8();
+            assert_eq!(v, 1u8);
+        }
+
+        if let Some(item) = leaf.find(4) {
+            let v = item.into_u8();
+            assert_eq!(v, 4u8);
+        }
+
+        let is_none = leaf.find(9);
+        assert_eq!(is_none, None);
+    }
+
+    #[test]
+    fn find_in_tree() {
+        let leaf1 = Node::new_leaf(0, vec![2, 3], vec![Record::from_u8(2), Record::from_u8(3)]);
+
+        let mut storage: MockNodeStorage = MockNodeStorage::new();
+        storage.add_node(&leaf1);
+        let res = find(&storage, &leaf1, 2);
+        assert!(res.is_some());
+        assert_eq!(res.unwrap().into_u8(), 2u8);
+
+        let leaf2 = Node::new_leaf(1, vec![1], vec![Record::from_u8(1)]);
+        storage.add_node(&leaf2);
+
+        let node1 = Node::new_root(2, vec![2], vec![Record::from_id(1), Record::from_id(0)]);
+
+        storage.add_node(&node1);
+        let res_1 = find(&storage, &node1, 1);
+        assert!(res_1.is_some());
+        assert_eq!(res_1.unwrap().into_u8(), 1u8);
+
+        let res_2 = find(&storage, &node1, 2);
+        assert!(res_2.is_some());
+        assert_eq!(res_2.unwrap().into_u8(), 2u8);
     }
 }
--- bpts/src/rec.rs
+++ bpts/src/rec.rs
@@ -1,34 +1,46 @@
 use crate::mfile;
+use crate::types;
 
-#[derive(Debug)]
-pub struct Record{
-    value:Vec<u8>
+use types::Id;
+
+#[derive(Debug, Clone)]
+pub struct Record {
+    value: Vec<u8>,
 }
 
-impl Record{
-
+impl Record {
     pub fn from_u8(value: u8) -> Self {
-        let mut v=Vec::new();
+        let mut v = Vec::new();
         v.push(value);
-        Record{value:v}
+        Record { value: v }
     }
-    
+
     pub fn from_i32(value: i32) -> Self {
-        let slice= unsafe { mfile::any_as_u8_slice(&value)} ;
-        Record{value:Vec::from(slice)}
-    }   
+        let slice = unsafe { mfile::any_as_u8_slice(&value) };
+        Record {
+            value: Vec::from(slice),
+        }
+    }
 
     pub fn into_u8(&self) -> u8 {
         assert!(!self.value.is_empty());
-       return self.value[0];
+        return self.value[0];
     }
 
     pub fn into_i32(&self) -> i32 {
-        let mut slice:[u8;4]=[0;4];
-        for i in 0..4{
-            slice[i]=self.value[i];
+        let mut slice: [u8; 4] = [0; 4];
+        for i in 0..4 {
+            slice[i] = self.value[i];
         }
         return i32::from_le_bytes(slice);
+    }
+
+    pub fn from_id(value: Id) -> Self {
+        Record::from_i32(value)
+    }
+
+    pub fn into_id(&self) -> Id {
+        self.into_i32()
     }
 }
 
@@ -44,24 +56,23 @@ impl PartialEq for Record {
     }
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn record_test(){
+    fn record_test() {
         {
-        let d1=Record::from_u8(1);
-        assert_eq!(d1.value[0], 1);
+            let d1 = Record::from_u8(1);
+            assert_eq!(d1.value[0], 1);
 
-        let value:u8 = d1.into_u8();
-        assert_eq!(value, 1);
+            let value: u8 = d1.into_u8();
+            assert_eq!(value, 1);
         }
 
         {
-            let d1=Record::from_i32(1i32);
-            let value:i32 = d1.into_i32();
+            let d1 = Record::from_i32(1i32);
+            let value: i32 = d1.into_i32();
             assert_eq!(value, 1i32);
         }
     }
