42c7ddddb3b4ebd161702c0b7bf83bc4468c2641
Sergey Lyubimov <lysevi@gmail.com>
2024-02-13 20:07:06 +0300

    utils:insert_to_array.

--- bpts/src/utils.rs
+++ bpts/src/utils.rs
@@ -1,3 +1,24 @@
 pub unsafe fn any_as_u8_slice<T: Sized>(p: &T) -> &[u8] {
     ::core::slice::from_raw_parts((p as *const T) as *const u8, ::core::mem::size_of::<T>())
 }
+
+pub fn insert_to_array<T>(target: &mut Vec<T>, pos: usize, value: T) {
+    let last = target.len();
+    for i in (1..last).rev() {
+        target.swap(i, i - 1);
+    }
+    target[pos] = value;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn find_in_leaf() {
+        let mut v = vec![1, 2, 3, 4, 0];
+        insert_to_array(&mut v, 0, 5);
+
+        assert_eq!(v, [5, 1, 2, 3, 4]);
+    }
+}
