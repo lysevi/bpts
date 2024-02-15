c1bcaf1df1da0d4aa1ab965d48330c4ccc2df31e
Любимов Сергей Викторович <sergey.lubimov@monitel.ru>
2024-02-15 15:26:21 +0300

    refact

--- bpts/src/lib.rs
+++ bpts/src/lib.rs
@@ -1,106 +1,14 @@
 pub mod mfile;
+pub mod node;
 pub mod rec;
 pub mod types;
 pub mod utils;
 
-use std::{cell::RefCell, collections::HashMap, rc::Rc};
+use std::{collections::HashMap, rc::Rc};
 
+use node::*;
 use rec::Record;
 use types::Id;
-
-pub type RcNode = Rc<RefCell<Node>>;
-
-#[derive(Clone)]
-pub struct Node {
-    pub id: Id, //TODO remove
-    pub is_leaf: bool,
-    pub parent: Id,
-    pub left: Id,
-    pub right: Id,
-    pub keys_count: usize,
-    pub data_count: usize,
-    pub keys: Vec<i32>,
-    pub data: Vec<Record>,
-}
-
-impl Node {
-    pub fn new(
-        id: Id,
-        is_leaf: bool,
-        keys: Vec<i32>,
-        data: Vec<Record>,
-        keys_count: usize,
-        data_count: usize,
-    ) -> RcNode {
-        Rc::new(RefCell::new(Node {
-            id: id,
-            is_leaf: is_leaf,
-            keys: keys,
-            data: data,
-            keys_count,
-            data_count,
-            left: 0,
-            parent: 0,
-            right: 0,
-        }))
-    }
-
-    pub fn new_root(
-        id: Id,
-        keys: Vec<i32>,
-        data: Vec<Record>,
-        keys_count: usize,
-        data_count: usize,
-    ) -> RcNode {
-        Node::new(id, false, keys, data, keys_count, data_count)
-    }
-
-    pub fn new_leaf(
-        id: Id,
-        keys: Vec<i32>,
-        data: Vec<Record>,
-        keys_count: usize,
-        data_count: usize,
-    ) -> RcNode {
-        Node::new(id, true, keys, data, keys_count, data_count)
-    }
-
-    pub fn can_insert(&self, t: usize) -> bool {
-        return self.data_count < (2 * t - 1);
-    }
-
-    pub fn is_empty(&self) -> bool {
-        return self.keys_count == 0;
-    }
-
-    pub fn find(&self, key: i32) -> Option<&Record> {
-        if key < self.keys[0] {
-            return self.data.first();
-        }
-
-        if !self.is_leaf {
-            if self.keys[self.keys_count - 1] <= key {
-                return Some(&self.data[self.data_count - 1]);
-            }
-        }
-
-        for i in 0..self.keys.len() {
-            match (self.keys[i]).cmp(&key) {
-                std::cmp::Ordering::Less => continue,
-                std::cmp::Ordering::Equal => return Some(&self.data[i]),
-                std::cmp::Ordering::Greater => return Some(&self.data[i]),
-            }
-        }
-        return None;
-    }
-
-    pub fn insert_data(&mut self, index: usize, key: i32, value: rec::Record) {
-        utils::insert_to_array(&mut self.keys, index, key);
-        utils::insert_to_array(&mut self.data, index, value);
-        self.keys_count += 1;
-        self.data_count += 1;
-    }
-}
 
 pub trait NodeStorage {
     fn get_new_id(&self) -> i32;
@@ -108,36 +16,6 @@ pub trait NodeStorage {
     fn get_node(&self, id: Id) -> Result<RcNode, types::Error>;
     //TODO add_node(node) -> ptr
     fn add_node(&mut self, node: &RcNode);
-}
-
-pub struct MockNodeStorage {
-    nodes: HashMap<Id, RcNode>,
-}
-
-impl MockNodeStorage {
-    pub fn new() -> MockNodeStorage {
-        MockNodeStorage {
-            nodes: HashMap::new(),
-        }
-    }
-}
-impl NodeStorage for MockNodeStorage {
-    fn get_new_id(&self) -> i32 {
-        let max = self.nodes.keys().into_iter().max_by(|x, y| x.cmp(y));
-        match max {
-            Some(x) => x + 1,
-            None => 1,
-        }
-    }
-    fn get_node(&self, id: Id) -> Result<RcNode, types::Error> {
-        let r = self.nodes.get(&id);
-        Ok(Rc::clone(r.unwrap()))
-    }
-
-    fn add_node(&mut self, node: &RcNode) {
-        let ref_node = node.borrow();
-        self.nodes.insert(ref_node.id, node.clone());
-    }
 }
 
 pub fn scan<'a>(
@@ -189,60 +67,11 @@ pub fn find<'a>(
     }
 }
 
-pub fn insert<'a>(
+pub fn split_node(
     storage: &mut dyn NodeStorage,
     root: &RcNode,
-    key: i32,
-    value: &Record,
     t: usize,
 ) -> Result<RcNode, types::Error> {
-    {
-        //TODO! extract method
-        let target_node: RcNode;
-        if root.borrow().is_empty() {
-            target_node = root.clone();
-        } else {
-            let scan_result = scan(storage, &root, key);
-            if scan_result.is_err() {
-                return Err(scan_result.err().unwrap());
-            }
-
-            target_node = scan_result.unwrap();
-        }
-        let mut mut_ref = target_node.borrow_mut();
-        let can_insert = mut_ref.can_insert(t);
-
-        // if mut_ref.is_leaf {
-        let mut index = mut_ref.keys_count;
-        for i in 0..mut_ref.keys_count {
-            if mut_ref.keys[i] > key {
-                index = i;
-                break;
-            }
-
-            if mut_ref.keys[i] == 0 {
-                index = i;
-                break;
-            }
-        }
-        mut_ref.insert_data(index, key, value.clone());
-        // } else {
-        //     let keys_count = mut_ref.keys_count;
-        //     let data_count = mut_ref.data_count;
-        //     if mut_ref.keys[keys_count] == key {
-        //         mut_ref.data[data_count] = value.clone();
-        //         mut_ref.data_count += 1;
-        //     } else {
-        //         for i in 0..keys_count {
-        //             if mut_ref.keys[i] > key {}
-        //         }
-        //     }
-        // }
-        if can_insert {
-            return Ok(root.clone());
-        }
-    }
-
     let mut parent_node: RcNode;
     let mut ref_root = root.borrow_mut();
     let is_new_root;
@@ -264,7 +93,7 @@ pub fn insert<'a>(
         parent_node = storage.get_node(ref_root.parent).unwrap();
     }
 
-    let mut new_brother: RcNode;
+    let new_brother: RcNode;
     let new_id = storage.get_new_id();
     let mut new_keys = vec![0i32; ref_root.keys.capacity()];
     let mut new_data = Record::empty_array(ref_root.data.len(), ref_root.data[0].size());
@@ -320,42 +149,82 @@ pub fn insert<'a>(
     }
 }
 
+pub fn insert(
+    storage: &mut dyn NodeStorage,
+    root: &RcNode,
+    key: i32,
+    value: &Record,
+    t: usize,
+) -> Result<RcNode, types::Error> {
+    {
+        //TODO! extract method
+        let target_node: RcNode;
+        if root.borrow().is_empty() {
+            target_node = root.clone();
+        } else {
+            let scan_result = scan(storage, &root, key);
+            if scan_result.is_err() {
+                return Err(scan_result.err().unwrap());
+            }
+
+            target_node = scan_result.unwrap();
+        }
+        let mut mut_ref = target_node.borrow_mut();
+        let can_insert = mut_ref.can_insert(t);
+
+        let mut index = mut_ref.keys_count;
+        for i in 0..mut_ref.keys_count {
+            if mut_ref.keys[i] > key {
+                index = i;
+                break;
+            }
+
+            if mut_ref.keys[i] == 0 {
+                index = i;
+                break;
+            }
+        }
+        mut_ref.insert_data(index, key, value.clone());
+
+        if can_insert {
+            return Ok(root.clone());
+        }
+    }
+    return split_node(storage, root, t);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn find_in_leaf() {
-        let leaf = Node::new_leaf(
-            0,
-            vec![1, 2, 3, 4],
-            vec![
-                Record::from_u8(1),
-                Record::from_u8(2),
-                Record::from_u8(3),
-                Record::from_u8(4),
-            ],
-            4,
-            4,
-        );
-        let ref_leaf = leaf.borrow();
-        if let Some(item) = ref_leaf.find(2) {
-            let v = item.into_u8();
-            assert_eq!(v, 2u8);
+    pub struct MockNodeStorage {
+        nodes: HashMap<Id, RcNode>,
+    }
+
+    impl MockNodeStorage {
+        pub fn new() -> MockNodeStorage {
+            MockNodeStorage {
+                nodes: HashMap::new(),
+            }
+        }
+    }
+    impl NodeStorage for MockNodeStorage {
+        fn get_new_id(&self) -> i32 {
+            let max = self.nodes.keys().into_iter().max_by(|x, y| x.cmp(y));
+            match max {
+                Some(x) => x + 1,
+                None => 1,
+            }
+        }
+        fn get_node(&self, id: Id) -> Result<RcNode, types::Error> {
+            let r = self.nodes.get(&id);
+            Ok(Rc::clone(r.unwrap()))
         }
 
-        if let Some(item) = ref_leaf.find(1) {
-            let v = item.into_u8();
-            assert_eq!(v, 1u8);
+        fn add_node(&mut self, node: &RcNode) {
+            let ref_node = node.borrow();
+            self.nodes.insert(ref_node.id, node.clone());
         }
-
-        if let Some(item) = ref_leaf.find(4) {
-            let v = item.into_u8();
-            assert_eq!(v, 4u8);
-        }
-
-        let is_none = ref_leaf.find(9);
-        assert_eq!(is_none, None);
     }
 
     #[test]
---
+++ bpts/src/node.rs
@@ -0,0 +1,141 @@
+use std::{cell::RefCell, collections::HashMap, rc::Rc};
+
+use crate::rec;
+use crate::types;
+use crate::utils;
+
+use rec::Record;
+use types::Id;
+
+pub type RcNode = Rc<RefCell<Node>>;
+
+#[derive(Clone)]
+pub struct Node {
+    pub id: Id, //TODO remove
+    pub is_leaf: bool,
+    pub parent: Id,
+    pub left: Id,
+    pub right: Id,
+    pub keys_count: usize,
+    pub data_count: usize,
+    pub keys: Vec<i32>,
+    pub data: Vec<Record>,
+}
+
+impl Node {
+    pub fn new(
+        id: Id,
+        is_leaf: bool,
+        keys: Vec<i32>,
+        data: Vec<Record>,
+        keys_count: usize,
+        data_count: usize,
+    ) -> RcNode {
+        Rc::new(RefCell::new(Node {
+            id: id,
+            is_leaf: is_leaf,
+            keys: keys,
+            data: data,
+            keys_count,
+            data_count,
+            left: 0,
+            parent: 0,
+            right: 0,
+        }))
+    }
+
+    pub fn new_root(
+        id: Id,
+        keys: Vec<i32>,
+        data: Vec<Record>,
+        keys_count: usize,
+        data_count: usize,
+    ) -> RcNode {
+        Node::new(id, false, keys, data, keys_count, data_count)
+    }
+
+    pub fn new_leaf(
+        id: Id,
+        keys: Vec<i32>,
+        data: Vec<Record>,
+        keys_count: usize,
+        data_count: usize,
+    ) -> RcNode {
+        Node::new(id, true, keys, data, keys_count, data_count)
+    }
+
+    pub fn can_insert(&self, t: usize) -> bool {
+        return self.data_count < (2 * t - 1);
+    }
+
+    pub fn is_empty(&self) -> bool {
+        return self.keys_count == 0;
+    }
+
+    pub fn find(&self, key: i32) -> Option<&Record> {
+        if key < self.keys[0] {
+            return self.data.first();
+        }
+
+        if !self.is_leaf {
+            if self.keys[self.keys_count - 1] <= key {
+                return Some(&self.data[self.data_count - 1]);
+            }
+        }
+
+        for i in 0..self.keys.len() {
+            match (self.keys[i]).cmp(&key) {
+                std::cmp::Ordering::Less => continue,
+                std::cmp::Ordering::Equal => return Some(&self.data[i]),
+                std::cmp::Ordering::Greater => return Some(&self.data[i]),
+            }
+        }
+        return None;
+    }
+
+    pub fn insert_data(&mut self, index: usize, key: i32, value: rec::Record) {
+        utils::insert_to_array(&mut self.keys, index, key);
+        utils::insert_to_array(&mut self.data, index, value);
+        self.keys_count += 1;
+        self.data_count += 1;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn find_in_leaf() {
+        let leaf = Node::new_leaf(
+            0,
+            vec![1, 2, 3, 4],
+            vec![
+                Record::from_u8(1),
+                Record::from_u8(2),
+                Record::from_u8(3),
+                Record::from_u8(4),
+            ],
+            4,
+            4,
+        );
+        let ref_leaf = leaf.borrow();
+        if let Some(item) = ref_leaf.find(2) {
+            let v = item.into_u8();
+            assert_eq!(v, 2u8);
+        }
+
+        if let Some(item) = ref_leaf.find(1) {
+            let v = item.into_u8();
+            assert_eq!(v, 1u8);
+        }
+
+        if let Some(item) = ref_leaf.find(4) {
+            let v = item.into_u8();
+            assert_eq!(v, 4u8);
+        }
+
+        let is_none = ref_leaf.find(9);
+        assert_eq!(is_none, None);
+    }
+}
