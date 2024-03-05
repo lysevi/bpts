c030a76b8f760f1b3c80ed92b024c306dc93c317
Sergey Lyubimov <lysevi@gmail.com>
2024-03-05 20:13:53 +0300

    cli-random: elapsed time.

--- cli/random/src/main.rs
+++ cli/random/src/main.rs
@@ -1,28 +1,36 @@
+use std::io::Write;
+
 use bpts::*;
 use rand::prelude::*;
+use std::time::Instant;
 
 fn main() {
-    let t = 10;
-    let count = 100000;
-    let mut root_node = Node::new_leaf_with_size(Id(1), t);
-    let mut storage: MockNodeStorage = MockNodeStorage::new();
-    storage.add_node(&root_node);
-
+    let count = 50000;
     let mut rng = rand::thread_rng();
 
-    let mut nums: Vec<i32> = (1..count).collect();
+    let mut nums: Vec<i32> = (0..count).collect();
     nums.shuffle(&mut rng);
+    println!("nums: {:?}", nums.len());
+    for t in 4..500 {
+        print!("t:{}", t);
+        std::io::stdout().flush().unwrap();
+        let start = Instant::now();
+        let mut root_node = Node::new_leaf_with_size(Id(1), t);
+        let mut storage: MockNodeStorage = MockNodeStorage::new();
+        storage.add_node(&root_node);
 
-    println!("nums: {:?}", nums);
-    for i in nums {
-        let res = insert(&mut storage, &root_node, i, &Record::from_i32(i), t);
-        assert!(res.is_ok());
-        root_node = res.unwrap();
+        for i in &nums {
+            let res = insert(&mut storage, &root_node, *i, &Record::from_i32(*i), t);
+            assert!(res.is_ok());
+            root_node = res.unwrap();
 
-        let res = bpts::find(&mut storage, &root_node, i);
-        if res.is_err() {
-            println!("> not found {}", i);
+            let res = bpts::find(&mut storage, &root_node, *i);
+            if res.is_err() {
+                println!("> not found {}", i);
+            }
+            assert!(res.is_ok());
         }
-        assert!(res.is_ok());
+        let duration = start.elapsed();
+        println!("\tstorage size:{} \telapsed:{:?}", storage.size(), duration);
     }
 }
