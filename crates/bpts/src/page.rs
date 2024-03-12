8df00221c614094b875d4eea7b6e1f83642a0055
Sergey Lyubimov <lysevi@gmail.com>
2024-03-12 13:38:16 +0300

    trans list.

--- crates/bpts/src/page.rs
+++ crates/bpts/src/page.rs
@@ -1,12 +1,22 @@
+use std::collections::HashMap;
+
 use crate::prelude::*;
 use crate::transaction::Transaction;
-use crate::utils;
 
+/*Page:
+0:[HEAD]
+space:{
+    Trans1,Trans1.1,TransList,Trans2.1,TransList
+    ....
+    Data4,Data3,Data2,Data1
+}
+TransList: count_u32 [offset of transactions]
+*/
 #[repr(C, packed)]
 struct Header {
     pub id: u32,
-    pub trans_offset: usize,
-    pub trans_size: usize,
+    pub trans_list_offset: u32,
+    pub free_space_head: u32,
 }
 
 const HEADER_SIZE: usize = std::mem::size_of::<Header>();
@@ -16,9 +26,9 @@ struct DataList {}
 
 struct Page<'a> {
     buffer: &'a mut [u8],
+    space: *mut u8,
     hdr: *mut Header,
-
-    trans: Option<Transaction>,
+    trans: HashMap<u32, Transaction>,
 }
 
 impl<'a> Page<'a> {
@@ -26,19 +36,31 @@ impl<'a> Page<'a> {
         let result: Page;
 
         let h = buffer.as_mut_ptr() as *mut Header;
-        let t: Option<Transaction> = unsafe {
-            if (*h).trans_offset == 0 {
-                None
+        let space = unsafe { buffer.as_mut_ptr().add(HEADER_SIZE) };
+        let t: HashMap<u32, Transaction> = unsafe {
+            if (*h).trans_list_offset == 0 {
+                HashMap::new()
             } else {
-                let ptr = buffer.as_mut_ptr();
-                let trans_offset = ptr.add((*h).trans_offset);
-                Some(Transaction::from_buffer(trans_offset))
+                let mut ptr = space.add((*h).trans_list_offset as usize);
+                let count = ptr as *const u32;
+                let mut transes = HashMap::new();
+
+                for _i in 0..*count {
+                    ptr = ptr.add(std::mem::size_of::<u32>());
+                    let offset = ptr as *const u32;
+                    let cur_trans_offset = space.add(*offset as usize);
+                    let cur_trans = Transaction::from_buffer(cur_trans_offset, *offset);
+                    transes.insert(cur_trans.tree_id(), cur_trans);
+                }
+
+                transes
             }
         };
         result = Page {
             buffer: buffer,
             hdr: h,
             trans: t,
+            space: space,
         };
 
         return Ok(result);
@@ -46,17 +68,36 @@ impl<'a> Page<'a> {
 
     pub fn save_trans(&mut self, t: Transaction) {
         unsafe {
-            if self.trans.is_none() {
-                let offset = HEADER_SIZE;
-                let buff_ptr = self.buffer.as_mut_ptr().add(offset);
-                let writed_bytes = t.save_to(buff_ptr);
-                self.trans = Some(t);
-                (*self.hdr).trans_size = writed_bytes;
-                (*self.hdr).trans_offset = offset;
-            } else {
-                todo!();
+            let mut target = t;
+            let offset = (*self.hdr).free_space_head;
+            let ptr = self.space.add(offset as usize);
+            let writed_bytes = target.save_to(ptr, offset);
+            target.set_offset(offset, ptr);
+            (*self.hdr).free_space_head += writed_bytes;
+
+            let mut exists = false;
+
+            self.trans.insert(target.tree_id(), target);
+
+            (*self.hdr).trans_list_offset = (*self.hdr).free_space_head;
+
+            let mut ptr = self.space.add((*self.hdr).free_space_head as usize);
+
+            let count_ptr = ptr as *mut u32;
+            let count = self.trans.len() as u32;
+            std::ptr::copy(&count, count_ptr, 1);
+            for trans in self.trans.iter() {
+                ptr = ptr.add(std::mem::size_of::<u32>());
+                let offset_ptr = ptr as *mut u32;
+                let value = trans.1.offset();
+                std::ptr::copy(&value, offset_ptr, 1);
             }
+
+            let offset = std::mem::size_of::<u32>() * (self.trans.len() + 1);
+            (*self.hdr).free_space_head += offset as u32;
         }
+
+        //self.trans = Some(transes);
     }
 
     pub fn get_id(&self) -> u32 {
@@ -70,11 +111,15 @@ impl<'a> Page<'a> {
         }
     }
 
-    pub fn transaction(&self) -> Option<Transaction> {
-        match &self.trans {
-            Some(t) => Some(t.clone()),
-            None => None,
+    pub fn transaction(&self, tree_id: u32) -> Option<Transaction> {
+        if let Some(v) = self.trans.get(&tree_id) {
+            return Some(v.clone());
         }
+        None
+    }
+
+    pub fn trees_count(&self) -> usize {
+        self.trans.len()
     }
 }
 
@@ -98,14 +143,45 @@ mod tests {
         }
         {
             let mut page = Page::from_buf(&mut b)?;
-            let t = Transaction::new(3);
-            assert!(page.transaction().is_none());
+            let t = Transaction::new(3, 7);
+            assert!(page.transaction(7).is_none());
             page.save_trans(t);
-            assert_eq!(page.transaction().unwrap().id(), 3);
+            assert_eq!(page.transaction(7).unwrap().rev(), 3);
         }
         {
-            let page = Page::from_buf(&mut b)?;
-            assert_eq!(page.transaction().unwrap().id(), 3);
+            let mut page = Page::from_buf(&mut b)?;
+            assert_eq!(page.transaction(7).unwrap().rev(), 3);
+            assert_eq!(page.transaction(7).unwrap().tree_id(), 7);
+            assert_eq!(page.trees_count(), 1);
+
+            let t = Transaction::new(1, 8);
+            page.save_trans(t);
+            assert_eq!(page.transaction(8).unwrap().rev(), 1);
+            assert_eq!(page.transaction(8).unwrap().tree_id(), 8);
+            assert_eq!(page.trees_count(), 2);
+        }
+
+        {
+            let mut page = Page::from_buf(&mut b)?;
+            assert_eq!(page.trees_count(), 2);
+            assert_eq!(page.transaction(7).unwrap().rev(), 3);
+            assert_eq!(page.transaction(7).unwrap().tree_id(), 7);
+
+            assert_eq!(page.transaction(8).unwrap().rev(), 1);
+            assert_eq!(page.transaction(8).unwrap().tree_id(), 8);
+
+            let t = Transaction::new(2, 8);
+            page.save_trans(t);
+        }
+
+        {
+            let mut page = Page::from_buf(&mut b)?;
+            assert_eq!(page.trees_count(), 2);
+            assert_eq!(page.transaction(7).unwrap().rev(), 3);
+            assert_eq!(page.transaction(7).unwrap().tree_id(), 7);
+
+            assert_eq!(page.transaction(8).unwrap().rev(), 2);
+            assert_eq!(page.transaction(8).unwrap().tree_id(), 8);
         }
         return Ok(());
     }
--- crates/bpts/src/transaction.rs
+++ crates/bpts/src/transaction.rs
@@ -1,48 +1,70 @@
 #[repr(C, packed)]
 #[derive(Debug, Clone)]
 pub struct TransactionHeader {
-    id: u32,
+    rev: u32,
+    tree_id: u32,
     size: u32,
 }
-const TRANSACTION_HEADER_SIZE: usize = std::mem::size_of::<TransactionHeader>();
+const TRANSACTION_HEADER_SIZE: u32 = std::mem::size_of::<TransactionHeader>() as u32;
 
 #[derive(Debug, Clone)]
 pub struct Transaction {
     header: TransactionHeader,
     buffer: Option<*mut u8>,
+    offset: u32,
 }
 
 impl Transaction {
-    pub fn new(id: u32) -> Transaction {
-        let hdr = TransactionHeader { id, size: 0 };
+    pub fn new(rev: u32, tree_id: u32) -> Transaction {
+        let hdr = TransactionHeader {
+            rev,
+            tree_id,
+            size: 0,
+        };
         Transaction {
             header: hdr,
             buffer: None,
+            offset: 0u32,
         }
     }
 
-    pub unsafe fn from_buffer(buffer: *mut u8) -> Transaction {
+    pub unsafe fn from_buffer(buffer: *mut u8, offset: u32) -> Transaction {
         let ptr = buffer as *const TransactionHeader;
         let hdr = std::ptr::read(ptr);
         Transaction {
             header: hdr,
             buffer: Some(buffer),
+            offset: offset,
         }
     }
 
-    pub unsafe fn save_to(&self, buffer: *mut u8) -> usize {
+    pub unsafe fn save_to(&mut self, buffer: *mut u8, offset: u32) -> u32 {
         let src_ptr = &self.header as *const TransactionHeader;
         let dest_ptr = buffer as *mut TransactionHeader;
-        std::ptr::copy(src_ptr, dest_ptr, TRANSACTION_HEADER_SIZE);
+        std::ptr::copy(src_ptr, dest_ptr, 1);
+        self.offset = offset;
         return self.size();
     }
 
-    pub fn size(&self) -> usize {
+    pub fn size(&self) -> u32 {
         return TRANSACTION_HEADER_SIZE;
     }
 
-    pub fn id(&self) -> u32 {
-        self.header.id
+    pub fn rev(&self) -> u32 {
+        self.header.rev
+    }
+
+    pub fn tree_id(&self) -> u32 {
+        self.header.tree_id
+    }
+
+    pub fn offset(&self) -> u32 {
+        self.offset
+    }
+
+    pub fn set_offset(&mut self, offset: u32, buffer: *mut u8) {
+        self.offset = offset;
+        self.buffer = Some(buffer);
     }
 }
 
