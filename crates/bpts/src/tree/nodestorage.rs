e3fc8feddecd8a1d7e762cc6e50202740cd98f83
Sergey Lyubimov <lysevi@gmail.com>
2024-04-08 07:05:17 +0300

    clean.

--- crates/bpts/src/tree/nodestorage.rs
+++ crates/bpts/src/tree/nodestorage.rs
@@ -9,13 +9,9 @@ use crate::{
 pub trait NodeStorage {
     fn get_root(&self) -> Option<RcNode>;
     fn get_new_id(&self) -> Id;
-    //TODO get_node(ptr) -> Option<&Node>;
     fn get_node(&self, id: Id) -> crate::Result<RcNode>;
-    //TODO add_node(node) -> ptr
     fn add_node(&mut self, node: &RcNode);
-
     fn erase_node(&mut self, id: &Id);
-
     fn get_params(&self) -> &TreeParams;
     fn get_cmp(&self) -> &dyn KeyCmp;
 }
